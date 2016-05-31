7b7b60e54c109804f14d5c94d4499202763d0558
Vahan <vahan_a@mail.ru>
2016-05-31 20:52:59 +0400

    com.oddeye.storm

--- src/main/java/com/oddeye/storm/LearningStormBolt.java
+++ src/main/java/com/oddeye/storm/LearningStormBolt.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.lerningstorm;
+package com.oddeye.storm;
 
 import java.io.File;
 import java.io.FileWriter;
