7cf843c3f718ce7a8aae6a08b189699053cdb934
Vahan <vahan_a@mail.ru>
2016-06-04 13:25:06 +0400

    Config yaml

--- src/main/java/com/oddeye/storm/KafkaHbaseTopology.java
+++ src/main/java/com/oddeye/storm/KafkaHbaseTopology.java
@@ -27,19 +27,19 @@ import org.apache.storm.topology.TopologyBuilder;
  */
 public class KafkaHbaseTopology {
 
-    public static void main(String[] args) {        
-        System.out.println(args[0]);
+    public static void main(String[] args) {
         String Filename = args[0];
         Config topologyconf = new Config();
-        try {            
+        try {
             Yaml yaml = new Yaml();
             java.util.Map rs = (java.util.Map) yaml.load(new InputStreamReader(new FileInputStream(Filename)));
             topologyconf.putAll(rs);
         } catch (Exception e) {
             System.out.println(e);
         }
-        java.util.Map<String, Object> kafkaconf = (java.util.Map<String, Object>) topologyconf.get("kafka");
-        java.util.Map<String, Object> tconf = (java.util.Map<String, Object>) topologyconf.get("Topology");        
+
+        java.util.Map<String, Object> kafkaconf = (java.util.Map<String, Object>) topologyconf.get("Kafka");
+        java.util.Map<String, Object> tconf = (java.util.Map<String, Object>) topologyconf.get("Topology");
 
         TopologyBuilder builder = new TopologyBuilder();
 //        HBaseBolt
@@ -60,7 +60,7 @@ public class KafkaHbaseTopology {
 // property should be false
 //        kafkaConfig.forceFromStart = true;
 // set the kafka spout class
-           
+
         builder.setSpout("KafkaSpout", new KafkaSpout(kafkaConfig), Integer.parseInt(String.valueOf(tconf.get("SpoutParallelism_hint"))));
 
         builder.setBolt("KafkaOddeyeMsgToHbaseBolt",
@@ -71,7 +71,7 @@ public class KafkaHbaseTopology {
         Config conf = new Config();
         conf.setNumWorkers(Integer.parseInt(String.valueOf(tconf.get("NumWorkers"))));
         conf.put(Config.TOPOLOGY_DEBUG, true);
-        conf.setDebug(true);
+        conf.setDebug(Boolean.getBoolean(String.valueOf(tconf.get("Debug"))));
         try {
 // This statement submit the topology on remote cluster. // args[0] = name of topology StormSubmitter.
             StormSubmitter.submitTopology("KafkaHbaseTopology", conf, builder.createTopology());
