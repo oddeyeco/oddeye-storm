1ffd36e65e4ce14fb3eebc61f600eabe6631e9c8
vahan <vahan@vahan-pc>
2016-12-20 12:37:41 +0400

    null error

--- src/main/java/co/oddeye/storm/CompareBolt.java
+++ src/main/java/co/oddeye/storm/CompareBolt.java
@@ -360,7 +360,7 @@ public class CompareBolt extends BaseRichBolt {
             mtrscList.set(mtrsc);
             collector.ack(tuple);
         } catch (Exception ex) {
-            LOGGER.error("In bib try:"+globalFunctions.stackTrace(ex)+tuple.getValueByField("metric"));
+            LOGGER.error("In big try:"+globalFunctions.stackTrace(ex)+tuple.getValueByField("metric"));
             collector.ack(tuple);
         }
     }
