56ba831ef121f4a93a7fe6b52e518ebb5d5c9a4c
vahan <vahan@vahan-pc>
2017-10-27 10:36:38 +0400

    Send emale

--- src/main/java/co/oddeye/storm/core/SendToEmail.java
+++ src/main/java/co/oddeye/storm/core/SendToEmail.java
@@ -7,8 +7,11 @@ package co.oddeye.storm.core;
 
 import co.oddeye.core.OddeeyMetricMeta;
 import co.oddeye.core.OddeeySenderMetricMetaList;
+import co.oddeye.core.OddeyeMessage;
 import co.oddeye.core.globalFunctions;
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
@@ -16,11 +19,14 @@ import javax.mail.Authenticator;
 import javax.mail.Session;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Transport;
 
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -68,29 +74,37 @@ public class SendToEmail extends SendTo {
 //        LOGGER.warn("Sent for user " + targetuser.getValue().getEmail() + " to Email " + targetdata.size() + " Messages");
         Iterator<Map.Entry<Integer, OddeeyMetricMeta>> iter = targetdata.entrySet().iterator();
         String Text = "";
+        String HTML = "";
 
         while (iter.hasNext()) {
 
             Map.Entry<Integer, OddeeyMetricMeta> entry = iter.next();
 
             if (entry.getValue().getErrorState().getLevel() == -1) {
-                Text = "<div>" + Text + "<br>Mertic:" + entry.getValue().getName() + "<br>Tags:<br>" + entry.getValue().getDisplayTags("<br>") + " Already not in Error " + "</div>";
+                HTML = "<div>" + HTML + "<br>Mertic:" + entry.getValue().getName() + "<br>Tags:<br>" + entry.getValue().getDisplayTags("<br>") + " Already not in Error " + "</div>";
+                Text = "/n" + Text + "/nMertic:" + entry.getValue().getName() + "/nTags:/n" + entry.getValue().getDisplayTags("/n") + " Already not in Error " + "/n";
             } else {
-                Text = "<div>" + Text + "<br>Level For Metric:" + entry.getValue().getName() + "<br>Tags:<br>" + entry.getValue().getDisplayTags("<br>") + " " + entry.getValue().getErrorState().getStateName() + " to " + entry.getValue().getErrorState().getLevelName() + "</div>";
+                HTML = "<div>" + HTML + "<br>Level For Metric:" + entry.getValue().getName() + "<br>Tags:<br>" + entry.getValue().getDisplayTags("<br>") + " " + entry.getValue().getErrorState().getStateName() + " to " + entry.getValue().getErrorState().getLevelName() + "</div>";
+                Text = "/n" + Text + "/nLevel For Metric:" + entry.getValue().getName() + "/nTags:/n" + entry.getValue().getDisplayTags("/n") + " " + entry.getValue().getErrorState().getStateName() + " to " + entry.getValue().getErrorState().getLevelName() + "/n";
             }
 
             iter.remove();
         }
 //        LOGGER.warn(Text);
-        if (!Text.isEmpty()) {
+        if (!HTML.isEmpty()) {
             // Recipient's email ID needs to be mentioned.
             String to = targetdata.getTargetValue();
 //            LOGGER.warn("targetdata.getTargetValue(); " + targetdata.getTargetValue());
             try {
-                MimeMessage message = new MimeMessage(session);
+                MimeMessage message = new OddeyeMessage(session);
                 message.addHeader("Content-type", "text/HTML; charset=UTF-8");
                 message.addHeader("format", "flowed");
                 message.addHeader("Content-Transfer-Encoding", "8bit");
+
+                String pattern = "EEE, dd MMM yyyy HH:mm:ss Z";
+                SimpleDateFormat format = new SimpleDateFormat(pattern);
+                message.addHeader("Date", format.format(Calendar.getInstance().getTime()));
+
                 // Set From: header field of the header.
                 message.setFrom(new InternetAddress(from, "OddEye Analytic"));
                 message.setReplyTo(InternetAddress.parse(from, false));
@@ -101,8 +115,20 @@ public class SendToEmail extends SendTo {
                 // Set Subject: header field
                 message.setSubject("OddEye Report");
 
+                final MimeBodyPart textPart = new MimeBodyPart();
+                textPart.setContent(Text, "text/plain");
+                // HTML version
+                final MimeBodyPart htmlPart = new MimeBodyPart();
+                htmlPart.setContent("<html lang=\"en\"><body>"+HTML+"</body></html>", "text/html");
+
+                final Multipart mp = new MimeMultipart("alternative");
+                mp.addBodyPart(textPart);
+                mp.addBodyPart(htmlPart);
                 // Send the actual HTML message, as big as you like
-                message.setContent(Text, "text/html");
+                message.setContent(mp);
+
+                // Send the actual HTML message, as big as you like
+//                message.setContent(HTML, "text/html");
 //                message.setHeader("Message-ID", Math.random()+"@oddeye.co");
 
 // Send message
