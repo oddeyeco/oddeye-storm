55d5e6f4eb9c90ed2c00ef67bc8e4a30f6c89cba
vahan <vahan@vahan-pc>
2018-01-15 09:34:51 +0400

    if (endtime > 5000) {

--- src/main/java/co/oddeye/storm/CalcRulesBolt.java
+++ src/main/java/co/oddeye/storm/CalcRulesBolt.java
@@ -249,7 +249,7 @@ public class CalcRulesBolt extends BaseRichBolt {
             starttime = System.currentTimeMillis();
             Deferred.groupInOrder(deferreds).join();
             endtime = System.currentTimeMillis() - starttime;
-            if (endtime > 1000) {
+            if (endtime > 5000) {
                 LOGGER.warn("Rules join SLOW " + deferreds.size() + " Calced Map " + mtrsc.getCalcedRulesMap().size() + " Count " + CalendarObjRules.getTime() + " time: " + endtime + " Hash " + mtrsc.hashCode() + " Name:" + mtrsc.getName() + " host:" + mtrsc.getTags().get("host").getValue());
             } else {
                 LOGGER.info("Rules join SLOW " + deferreds.size() + " Calced Map " + mtrsc.getCalcedRulesMap().size() + " Count " + CalendarObjRules.getTime() + " time: " + endtime + " Hash " + mtrsc.hashCode() + " Name:" + mtrsc.getName() + " host:" + mtrsc.getTags().get("host").getValue());
